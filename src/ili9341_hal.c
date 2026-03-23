708a2056bc086808a7211ea99d7057d8799a46f6
Leonardo <leonardo.oseguera@hotmail.com>
2026-03-23 12:26:12 -0600

    Feat: Add halInitialized variable to avoid executing public interfaces without proper initialization

--- src/ili9341_hal.c
+++ src/ili9341_hal.c
@@ -17,7 +17,7 @@
 /********************************************************************************
  * EXTERN VARIABLES
  ********************************************************************************/
-
+bool halInitialized = false;
 /********************************************************************************
  * PRIVATE MACROS AND DEFINES
  ********************************************************************************/
@@ -45,7 +45,7 @@ static void write_cmd(uint8_t cmd)
 {
 	DC_CMD();
 	CHIP_SELECT();
-	spi_write(&cmd, 1);
+	mcal_spi_write(&cmd, 1);
 	CHIP_UNSELECT();
 }
 
@@ -53,74 +53,102 @@ static void write_data(const uint8_t *data, uint32_t len)
 {
 	DC_DATA();
 	CHIP_SELECT();
-	spi_write(data, len);
+	mcal_spi_write(data, len);
 	CHIP_UNSELECT();
 }
 
 /********************************************************************************
  * GLOBAL FUNCTIONS
  ********************************************************************************/
-void hal_delay_ms(uint16_t ms)
-{
-	mcal_delay_ms(ms);
-}
-
 void hal_comms_init(void)
 {
-	spi_init();
+	mcal_init();
+	if(mcalInitialized)
+	{
+		halInitialized = true;
+	}
+}
+
+void hal_delay_ms(uint16_t ms)
+{
+	if(halInitialized)
+	{
+		mcal_delay_ms(ms);
+	}
 }
 
 void hal_hardware_reset(void)
 {
-	// Hardware reset
-	//reset_hardware_display();
-	RST_LOW();
-	delay_ms(10);
-	RST_HIGH();
-	delay_ms(120);
+	if(halInitialized)
+	{
+		// Hardware reset
+		//reset_hardware_display();
+		RST_LOW();
+		mcal_delay_ms(10);
+		RST_HIGH();
+		mcal_delay_ms(120);
+	}
 }
 
 void hal_software_reset(void)
 {
-	//reset_software_display();
-	write_cmd(0x01); // Software reset
-	delay_ms(150);
+	if(halInitialized)
+	{
+		//reset_software_display();
+		write_cmd(0x01); // Software reset
+		mcal_delay_ms(150);
+	}
 }
 
 void hal_set_pixel_format_16bits()
 {
-	write_cmd(PIXEL_FORMAT_SET);
-	uint8_t pixelFormat[] = {0x55};
-	write_data(pixelFormat, 1);
+	if(halInitialized)
+	{
+		write_cmd(PIXEL_FORMAT_SET);
+		uint8_t pixelFormat[] = {0x55};
+		write_data(pixelFormat, 1);
+	}
 }
 
 void hal_sleep_out()
 {
-	write_cmd(SLEEP_OUT); // Sleep out
-	delay_ms(120);
+	if(halInitialized)
+	{
+		write_cmd(SLEEP_OUT); // Sleep out
+		mcal_delay_ms(120);
+	}
 }
 
 void hal_display_on()
 {
-	write_cmd(DISPLAY_ON);
+	if(halInitialized)
+	{
+		write_cmd(DISPLAY_ON);
+	}
 }
 
 void hal_set_column_limits(uint16_t startColumn, uint16_t endColumn)
 {
-	uint8_t startColumnData[2] = {startColumn >> 8, startColumn & 0x0F};
-	uint8_t endColumnData[2]   = {endColumn >> 8, endColumn & 0x0F};
+	if(halInitialized)
+	{
+		uint8_t startColumnData[2] = {startColumn >> 8, startColumn & 0x0F};
+		uint8_t endColumnData[2]   = {endColumn >> 8, endColumn & 0x0F};
 
-	write_cmd(COLUMN_ADDRESS_SET);
-	write_data(startColumnData, 2);
-	write_data(endColumnData, 2);
+		write_cmd(COLUMN_ADDRESS_SET);
+		write_data(startColumnData, 2);
+		write_data(endColumnData, 2);
+	}
 }
 
 void hal_set_row_limits(uint16_t startRow, uint16_t endRow)
 {
-	uint8_t startRowData[2] = {startRow >> 8, startRow & 0x0F};
-	uint8_t endRowData[2]   = {endRow >> 8, endRow & 0x0F};
+	if(halInitialized)
+	{
+		uint8_t startRowData[2] = {startRow >> 8, startRow & 0x0F};
+		uint8_t endRowData[2]   = {endRow >> 8, endRow & 0x0F};
 
-	write_cmd(PAGE_ADDRESS_SET);
-	write_data(startRowData, 2);
-	write_data(endRowData, 2);
+		write_cmd(PAGE_ADDRESS_SET);
+		write_data(startRowData, 2);
+		write_data(endRowData, 2);
+	}
 }
